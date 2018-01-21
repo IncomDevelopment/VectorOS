9eb3774de8627483bf103b7ed998b9628d59ae7a
LeoDog896 <LeoDog896@hotmail.com>
2018-01-21 17:45:09 -0500

    Update system.js
    Added 2 basic functions

--- system/terminal/system.js
+++ system/terminal/system.js
@@ -1,9 +1,22 @@
 var terminal = {
   addcmd: function(name,func){
-  
+    if (this.commands[name] === null || this.commands[name] === undefined){
+      this.commands[name] == func;
+      console.log("Added Command Suscessfully");
+      return true;
+    } else {
+      console.log("Error! Command Already Exists");
+      return false; 
+    }
   },
   input: function(string){
-  
+    if (this.commands[name] === null || this.commands[name] === undefined){
+      console.log("Error! Command Doesn't Exist!");
+    } else if (typeof this.commands[name] === 'function') {
+      this.commands[name](string);
+    } else {
+      console.log("Error! Command Isn't A Function!");
+    }
   },
   commands: []
 }
